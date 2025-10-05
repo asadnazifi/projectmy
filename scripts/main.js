6f1cea526036812b537b2ddb5616d72095ed563f
AsadNazifi <asad15nazifi@gmail.com>
2025-10-06 00:39:43 +0330

    fix nav menu and heafer

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
-document.getElementById('open-menu').addEventListener('click',function(){
-    alert('asdas');
-})
+const menuDiv = document.querySelector('.nav-menu div:first-child');
+const openBtn = document.getElementById('open-menu');
+const closeBtn = document.getElementById('close-menu');
+
+openBtn.addEventListener('click', function () {
+    // اول کلاس بستن رو حذف می‌کنیم
+    menuDiv.classList.remove('closing');
+    // نمایش حالت باز
+    menuDiv.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function () {
+    // حالت بستن
+    menuDiv.classList.remove('active');
+    menuDiv.classList.add('closing');
+
+    // بعد از پایان انیمیشن، کامل مخفی کنیم
+    setTimeout(() => {
+        menuDiv.classList.remove('closing');
+    }, 400); // برابر زمان transition
+});
